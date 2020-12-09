8840fc00754ca9d6e85aaeb12d3d8b4d7e712032
Amit <amitkumar.developer1@gmail.com>
2020-12-09 17:18:36 +0530

    Remove signup signin button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,6 @@ const Navbar = () => {
     return(
         <nav className="navbar navbar-dark bg-dark">
             <span className="navbar-brand text-white">Todo App</span>
-            <div>
-                <button className="btn btn-light mr-2" type="submit">SignUp</button>
-                <button className="btn btn-light" type="submit">Login</button>
-            </div>
         </nav>
     )
 }
